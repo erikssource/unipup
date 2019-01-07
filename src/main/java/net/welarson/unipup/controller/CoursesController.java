0022bdd25cd69b32f1ba3957378f55dc4b711cb0
Erik Larson <erik@welarson.com>
2019-01-07 10:16:38 -0600

    Update CORS

--- src/main/java/net/welarson/unipup/controller/CoursesController.java
+++ src/main/java/net/welarson/unipup/controller/CoursesController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,12 @@ public class CoursesController {
 	@GetMapping("/{id}")
 	public Course findById(@PathVariable String id) {
 		return repo.findEntity(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteById(@PathVariable String id) {
+		repo.delete(id);
 	}
 	
 	@PostMapping()
